b11a781fae908da074d4fc073cc247eafc89dffa
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-16 03:30:45 +0800

    Create result.sh

---
+++ result.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ip=`curl ifconfig.me`
+connectCount=`curl -s http://localhost:1635/peers | jq '.peers | length' `
+address=`curl localhost:1635/addresses | jq | grep ethereum`
+balance=`curl localhost:1635/chequebook/balance | jq`
+cheque=`curl localhost:1635/chequebook/cheque | jq`
+
+echo $ip
+echo $connectCount
+echo $address
+echo $balance
+echo $cheque
