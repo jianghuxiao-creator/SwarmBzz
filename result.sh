4efe4f4844606076207f3b051ba3608610caaa74
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-16 04:52:58 +0800

    Update result.sh

--- result.sh
+++ result.sh
@@ -2,12 +2,31 @@
 ip=`curl ifconfig.me`
 connectCount=`curl -s http://localhost:1635/peers | jq '.peers | length' `
 address=`curl localhost:1635/addresses | jq | grep ethereum`
-address=`curl localhost:1635/addresses | jq | grep ethereum`
+publicKey=`curl localhost:1635/addresses | jq | grep publicKey`
+pssPublicKey=`curl localhost:1635/addresses | jq | grep pssPublicKey`
 balance=`curl localhost:1635/chequebook/balance | jq`
 cheque=`curl localhost:1635/chequebook/cheque | jq`
 
-echo $ip
-echo $connectCount
-echo $address
-echo $balance
-echo $cheque
+mkdir logs
+echo $ip >>/home/bee/logs/bee_console.log>&1
+echo $connectCount >>/home/bee/logs/bee_console.log>&1
+echo $address >>/home/bee/logs/bee_console.log>&1
+echo $publicKey >>/home/bee/logs/bee_console.log>&1
+echo $pssPublicKey >>/home/bee/logs/bee_console.log>&1
+echo $balance >>/home/bee/logs/bee_console.log>&1
+echo $cheque >>/home/bee/logs/bee_console.log>&1
+
+apt -y install expect
+expect -c "
+spawn scp -r /home/bee/logs  root@8.208.115.2:/home
+expect {   
+    \"*assword\"
+                {
+                    set timeout 300;
+                    send \"YcYsf88*\r\";
+                }
+    \"yes/no\"
+                {
+                    send \"yes\r\"; exp_continue;}
+                }
+expect eof"
