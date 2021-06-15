e1b641a2eb5ebb073122ae75b2225d963a96463a
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-16 07:03:46 +0800

    Update result.sh

--- result.sh
+++ result.sh
@@ -11,7 +11,7 @@ cheque=`curl localhost:1635/chequebook/cheque | jq`
 mkdir logs
 a=`curl ifconfig.me`
 echo $ip >>/home/bee/logs/${a}-bee_console.log>&1
-echo $connectCount >>/home/${a}-bee/logs/bee_console.log>&1
+echo $connectCount >>/home/bee/logs/${a}-bee_console.log>&1
 echo $address >>/home/bee/logs/${a}-bee_console.log>&1
 echo $publicKey >>/home/bee/logs/${a}-bee_console.log>&1
 echo $pssPublicKey >>/home/bee/logs/${a}-bee_console.log>&1
