cd9961972a289b8cc594a65ec021d5ce335fa6a0
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-16 04:02:21 +0800

    Update result.sh

--- result.sh
+++ result.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-ip=`curl ifconfig.me`
-connectCount=`curl -s http://localhost:1635/peers | jq '.peers | length' `
-address=`curl localhost:1635/addresses | jq | grep ethereum`
-balance=`curl localhost:1635/chequebook/balance | jq`
-cheque=`curl localhost:1635/chequebook/cheque | jq`
+ip=`curl ifconfig.me`
+connectCount=`curl -s http://localhost:1635/peers | jq '.peers | length' `
+address=`curl localhost:1635/addresses | jq | grep ethereum`
+address=`curl localhost:1635/addresses | jq | grep ethereum`
+balance=`curl localhost:1635/chequebook/balance | jq`
+cheque=`curl localhost:1635/chequebook/cheque | jq`
 
-echo $ip
-echo $connectCount
-echo $address
-echo $balance
-echo $cheque
+echo $ip
+echo $connectCount
+echo $address
+echo $balance
+echo $cheque
