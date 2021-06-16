84b6bc6df502c8162fe4b150141659534a11a1e9
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-16 14:27:04 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-#test.sh
 #!/bin/bash
 
+rm -r /home/logs
 dir=/home/bee/result
 
 while read line
@@ -8,4 +8,10 @@ do
 host=`echo $line| awk '{print $1}'`
 passwd=`echo $line | awk '{print $2}'`
 $dir/expect_ssh_test.sh $host $passwd &
+done < $dir/host.txt
+while read line
+do
+host=`echo $line| awk '{print $1}'`
+passwd=`echo $line | awk '{print $2}'`
+$dir/passlocal.sh $host $passwd &
 done < $dir/host.txt
