e21322f22876617af2c6fb0afe197786a46fbf44
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-16 10:51:42 +0800

    Create pass.sh

---
+++ pass.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env expect
+
+spawn scp -r /home/bee/logs  root@8.208.115.2:/home
+expect {   
+    \"*assword\"
+                {
+                    set timeout 300;
+                    send \"YcYsf88*\r\";
+                }
+    \"yes/no\"
+                {
+                    send \"yes\r\"; exp_continue;}
+                }
+expect eof"
