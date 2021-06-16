7b4dbce415e3930b8d921406f070878b29f14a52
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-16 10:06:13 +0800

    Update result.sh

--- result.sh
+++ result.sh
@@ -18,18 +18,3 @@ echo $publicKey >>/home/bee/logs/${a}-bee_console.log>&1
 echo $pssPublicKey >>/home/bee/logs/${a}-bee_console.log>&1
 echo $balance >>/home/bee/logs/${a}-bee_console.log>&1
 echo $cheque >>/home/bee/logs/${a}-bee_console.log>&1
-
-apt -y install expect
-expect -c "
-spawn scp -r /home/bee/logs  root@8.208.115.2:/home
-expect {   
-    \"*assword\"
-                {
-                    set timeout 300;
-                    send \"YcYsf88*\r\";
-                }
-    \"yes/no\"
-                {
-                    send \"yes\r\"; exp_continue;}
-                }
-expect eof"
