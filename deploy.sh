ed8af1e552e3a3133c5ecd7065a7d43bd4e030ca
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-24 22:44:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,27 @@
-apt-get update
-apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --y -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --y -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo Y | apt-get install docker-ce docker-ce-cli containerd.io
+echo Y | sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 docker -v
 
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
 
