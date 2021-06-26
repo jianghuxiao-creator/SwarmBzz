c9ad3537b2c4b8bd73e21819e22b870bd73647d4
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-27 00:44:27 +0800

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -12,9 +12,9 @@ docker-compose ps >log.txt 2>&1
 
 apt install -y jq
 
-python3 dblink.py
+python3 dblink.py && rm aa
 
-rm aa
+
 crontab -l > aa
 echo "*/1 * * * * python3 /home/mainnet/dblink.py" >> aa
 crontab aa
