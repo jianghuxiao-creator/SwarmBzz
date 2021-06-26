2caa1e311ff03b21177b0d5303fa60d5bd9edf3e
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-26 22:08:16 +0800

    Create watch.sh

--- watch.sh
+++ watch.sh
@@ -1,11 +1,14 @@
 pip install pymysql
 
+rm dblink.py
+
 wget https://raw.githubusercontent.com/luzhongyun/SwarmBzz/main/dblink.py
 
 docker-compose ps >log.txt 2>&1
 
 python3 dblink.py
 
+rm aa
 crontab -l > aa
 echo "*/1 * * * * python3 /home/mainnet/dblink.py" >> aa
 crontab aa
