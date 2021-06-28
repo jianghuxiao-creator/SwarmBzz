3d1b11c8cf34f6d475125a8117eae938321f13bc
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-06-28 13:30:49 +0800

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -19,3 +19,15 @@ fi
 
 python3 dblink.py
 
+FIND_FILE="/home/mainnet/aa"
+FIND_STR="dblink.py"
+crontab -l > aa
+# 判断匹配函数，匹配函数不为0，则包含给定字符
+if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
+    echo "定时任务已设置 python3 /home/mainnet/dblink.py!"
+else
+	echo "定时任务未设置"
+	echo "*/1 * * * * python3 /home/mainnet/dblink.py" >> aa
+	crontab aa
+	service cron restart
+fi
