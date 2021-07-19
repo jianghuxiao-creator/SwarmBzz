3d5497ac7e706f755c8f5043c717f54726ff5d9b
luzhongyun <37952391+luzhongyun@users.noreply.github.com>
2021-07-19 17:34:34 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get update
+
+echo Y | sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+docker -v
+
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+
+
+cd /home
+mkdir -p bee && cd bee
+
+
+wget -q https://raw.githubusercontent.com/luzhongyun/SwarmBzz/main/docker-compose.yml
+
+wget -q https://raw.githubusercontent.com/luzhongyun/SwarmBzz/main/.env
+docker-compose up -d
